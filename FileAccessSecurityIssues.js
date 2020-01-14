f6f84a39a7e69237e3f81b1f560e7a932aa51f48
devcodacytestuser <58774704+devcodacytestuser@users.noreply.github.com>
2020-01-14 10:17:31 +0000

    Create FileAccessSecurityIssues.js

---
+++ FileAccessSecurityIssues.js
@@ -0,0 +1,3 @@
+var tab = window.open(url + "/settings/applications/new", '_blank');
+var tab = window.open(url + "/plugins/servlet/applinks/listApplicationLinks", '_blank');
+var tab = window.open(url + "/admin/applications", '_blank');
