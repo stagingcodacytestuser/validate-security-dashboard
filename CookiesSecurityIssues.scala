beb059fa45b1e24fa770c5fb8fc810bc592c3b06
devcodacytestuser <58774704+devcodacytestuser@users.noreply.github.com>
2020-01-14 13:52:53 +0000

    Update CookiesSecurityIssues.scala

--- CookiesSecurityIssues.scala
+++ CookiesSecurityIssues.scala
@@ -1,40 +1,17 @@
-trait StateVerification extends Logger {
-  def verifySessionState(provider: Provider.Value, request: Request[AnyContent]): Boolean = {
-    provider match {
-      case Provider.Bitbucket | Provider.Stash => true
-      case _ =>
-        if (WebsiteConfiguration.isOAuthStateEnable) {
-          request.session.get("sessionId").exists { sessionId =>
-            val stateMatched = request.getQueryString("state").exists { state =>
-              accountDB.deprecated.LoginSessionsTable
-                .getBySession(sessionId, WebsiteConfiguration.loginStateDuration.toMillis)
-                .contains(state)
-            }
-            accountDB.deprecated.LoginSessionsTable.deleteSession(sessionId)
-            stateMatched
-          }
-        } else {
-          true
-        }
-    }
-  }
-  
-  redirectToWebLogin().withNewSession
-        .flashing("success" ->
-          "Thanks for signing up. We're currently under private beta but we'll notify you as soon as we go live.")
-        .discardingCookies(DiscardingCookie(CookieHelper.providerCookieKey), CookieHelper.autoLoginDiscardingCookie)
+class AuthController()(implicit eventBus: EventBus) extends Controller with Secured with RedirectUtils with Logger {
+ lazy val autoLoginCookie = Cookie(name = autoLoginKey,
+  value = "true",
+  maxAge = Option(autoLoginExpiration),
+  httpOnly = false,
+  domain = autoLoginDomain)
 
-lazy val autoLoginCookie = Cookie(name = autoLoginKey,
-                                    value = "true",
-                                    maxAge = Option(autoLoginExpiration),
-                                    httpOnly = false,
-                                    domain = autoLoginDomain)
+ lazy val autoLoginDiscardingCookie = DiscardingCookie(name = autoLoginKey, domain = autoLoginDomain)
 
-  lazy val autoLoginDiscardingCookie = DiscardingCookie(name = autoLoginKey, domain = autoLoginDomain)
+ def withProviderCookie(result: Result, provider: Provider.Value): Result = {
+  val providerCookie =
+  Cookie(providerCookieKey, provider.toString, Option(providerCookieExpiration), httpOnly = false)
 
-  def withProviderCookie(result: Result, provider: Provider.Value): Result = {
-    val providerCookie =
-      Cookie(providerCookieKey, provider.toString, Option(providerCookieExpiration), httpOnly = false)
+  result.withCookies(autoLoginCookie, providerCookie)
+ }
 
-    result.withCookies(autoLoginCookie, providerCookie)
-  }
+}
